c98ab04309c9becd1189947019cb423e8c6a237c
Iulia Elisa <iuliaelisa15@gmail.com>
2024-05-09 11:59:46 +0300

    Refactor YOLO and SAM training scripts

--- run_yolo_splits_train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-iterations=(1 2 3 4)
-
-for iter in "${iterations[@]}"; do
-	log_file="train_log_yolo_${iter}.log"
-	echo "Running ft_YOLO_V8_segm.py with iter=${iter}..."
-	nohup python ft_YOLO_V8_segm.py ${iter} > $log_file 2>&1 &
-done
--- train/run_sam_splits_train.sh
+++ train/run_sam_splits_train.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-iterations=(1 2 3 4)
+sleep 7200 # run script after 2 hours
+
+iterations=(0 1 2 3)
 device_ids=(0 1 2 3)
 
 gpu=1
 for iter in "${iterations[@]}"; do
-	log_file="train_log_sam_${iter}.log"
+	log_file="train_sam_${iter}.log"
 	echo "Running train_sam.py with iter=${iter}..."
 	nohup python train_sam.py ${iter} ${gpu} > $log_file 2>&1 &
 	gpu=$((gpu + 1))
