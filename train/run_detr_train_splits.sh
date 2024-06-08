c33a86400a1808a31b3e822a5a04de4849a5d47b
Iulia Elisa <iuliaelisa15@gmail.com>
2024-06-08 11:53:34 +0300

    small refactory for detector shell scripts

--- train/run_detr_train_splits.sh
+++ train/run_detr_train_splits.sh
@@ -5,7 +5,7 @@
 skf_iterations=(0) # List of iteration indices from Stratified K-Fold
 
 for iter in "${skf_iterations[@]}"; do
-	log_file="train_detector_${iter}.log" # Log file for output redirection
+	log_file="train_yolo_${iter}.log" # Log file for output redirection
 	echo "Running train_detector.py with iter=${iter}..."
 	# Run the training script in the background
 	nohup python train_detector.py ${iter} > $log_file 2>&1 &
