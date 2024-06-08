636ad224b4e63b15380c9046dc2284121c72c995
Iulia Elisa <iuliaelisa15@gmail.com>
2024-06-08 11:56:34 +0300

    modified parameter in yolo training

--- train/train_detector.py
+++ train/train_detector.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO, RTDETR
-import yaml
 import sys
 import os
 
@@ -21,41 +20,10 @@ data_yaml_path = yolo_dataset_path+'data.yaml'
 device = 2
 
 # Train YOLOv8-segm model
-# model_checkpoint = 'yolov8n-seg.pt'
-# model = YOLO(model_checkpoint) 
+model_checkpoint = 'yolov8n-seg.pt'
+model = YOLO(model_checkpoint) 
 
-# project = f"yolov8-segm-{iter}" 
-# name = model_checkpoint.replace('.pt', '') 
-
-# Train the model
-# results = model.train(data=data_yaml_path,
-#                       project=project,
-#                       name=name,
-# 					  task='detect',
-#                       epochs=300,
-#                       patience=0, # patience=0 disables early stopping
-#                       batch=16,
-#                       imgsz=512,
-#                       device=1,
-# 					  hsv_h=0.0, hsv_s=0.0, hsv_v=0.0,
-# 					  lr0=0.0006,
-# 					  dropout=0.2,
-# 					  mask_ratio = 1,
-# 					  mosaic=0,
-# 					  # cos_lr=True,
-# 					  box=0.9,
-# 					  cls=0.8,
-# 					  # label_smoothing=0.1,
-#                       # augment=True, 
-#                       # freeze layers
-#                      )
-
-# Train RT-DETR
-# Load a COCO-pretrained RT-DETR-l model
-model_checkpoint = 'rtdetr-l.pt'
-model = RTDETR(model_checkpoint)
-
-project = f"rt-detr-iter{iter}" 
+project = f"yolov8-segm-{iter}" 
 name = model_checkpoint.replace('.pt', '') 
 
 # Train the model
@@ -65,18 +33,48 @@ results = model.train(data=data_yaml_path,
 					  task='detect',
                       epochs=300,
                       patience=0, # patience=0 disables early stopping
-                      batch=32,
+                      batch=16,
                       imgsz=512,
                       device=device,
 					  hsv_h=0.0, hsv_s=0.0, hsv_v=0.0,
-					  lr0=0.0006/2,
+					  lr0=0.0006,
 					  dropout=0.2,
 					  mask_ratio = 1,
 					  mosaic=0,
-					  cos_lr=True,
+					  # cos_lr=True,
 					  box=0.9,
 					  cls=0.8,
-					  label_smoothing=0.1,
-                      augment=True, 
+					  # label_smoothing=0.1,
+                      # augment=True, 
                       # freeze layers
                      )
+
+# # Train RT-DETR
+# # Load a COCO-pretrained RT-DETR-l model
+# model_checkpoint = 'rtdetr-l.pt'
+# model = RTDETR(model_checkpoint)
+
+# project = f"rt-detr-iter{iter}" 
+# name = model_checkpoint.replace('.pt', '') 
+
+# # Train the model
+# results = model.train(data=data_yaml_path,
+#                       project=project,
+#                       name=name,
+# 					  task='detect',
+#                       epochs=300,
+#                       patience=0, # patience=0 disables early stopping
+#                       batch=32,
+#                       imgsz=512,
+#                       device=device,
+# 					  hsv_h=0.0, hsv_s=0.0, hsv_v=0.0,
+# 					  lr0=0.0006/2,
+# 					  dropout=0.2,
+# 					  mask_ratio = 1,
+# 					  mosaic=0,
+# 					  cos_lr=True,
+# 					  box=0.9,
+# 					  cls=0.8,
+# 					  label_smoothing=0.1,
+#                       augment=True, 
+#                      )
